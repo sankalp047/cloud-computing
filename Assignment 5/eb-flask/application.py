f5596becd3d58a0e0c54e81731833aa97dab469b
Naman Jain <namanjain.vimalkumar@gmail.com>
2019-06-24 21:39:38 -0500

    clusters with respect to numerical values completed

--- Assignment 5/eb-flask/application.py
+++ Assignment 5/eb-flask/application.py
@@ -27,20 +27,86 @@ application = Flask(__name__)
 def view_index():
     return render_template("index.html")
 
+def normalizeData(X):
+    mean_X = np.mean(X, axis=0)
+    std_X = np.std(X, axis=0)
+    new_X = (X-mean_X)/std_X
+    return {
+        "new_X": new_X,
+        "mean": mean_X,
+        "std": std_X
+    }
+
+def computeX(Z, mean, std):
+    # return np.multiply(Z, std) + mean
+    return Z*std + mean
+
+def getTableValues(data, clusters, labels):
+    print(data)
+    print("----- Labels ------")
+    print(labels)
+    cluster_details = []
+    cluster_count = np.shape(clusters)[0]
+    for i in range(cluster_count):
+        this_cluster_indices = np.where(labels == i)
+        this_cluster_points = data[this_cluster_indices,:]
+        print("-----  "+str(i)+"  ------")
+        print(this_cluster_points)
+        this_distance = np.sqrt(np.sum((this_cluster_points - clusters[i,:])**2))
+        this_details = {
+            "index": i + 1,
+            "Centroid": str(clusters[i,:]), 
+            "Distance": str(this_distance), 
+            "Count": np.shape(this_cluster_points)[1],
+            "Points": {
+                "x": this_cluster_points[0,:,0].tolist(), 
+                "y": this_cluster_points[0,:,1].tolist(),
+            }
+        }
+        cluster_details.append(this_details)
+        print("Centroid: " + str(clusters[i,:]) + "..... Closely Packed: "+ str(this_distance) + "..... Cluster Size: " + str(np.shape(this_cluster_points)[1]))
+        # print(np.shape(this_cluster_points))
+
+    return cluster_details
+
 def view_cluster():
     cluster_count = int(request.form.get("cluster_count"))
     clusters = request.form.get("checked").split(",")
 
-    q = "SELECT " + str(request.form.get("checked")) + " FROM titanic3 LIMIT 10"
-
+    q = "SELECT " + str(request.form.get("checked")) + " FROM titanic3 WHERE "
+    for c in clusters:
+        q += " " + str(c) + " IS NOT NULL AND"
+    q = q[:len(q)-3]
     mycursor = mydb.cursor()
     mycursor.execute(q)
-    data = np.array(mycursor.fetchall())
-    print(data.shape)
-    kmeans = KMeans(n_clusters=cluster_count, random_state=0).fit(data)
-    print(kmeans.cluster_centers_)
+    records = mycursor.fetchall()
 
-    return render_template("index.html")
+    r = np.array(records)
+    table = normalizeData(np.array(r))
+    r = table.get("new_X")
+    # print(computeX(data, table.get("mean"), table.get("std")))
+    # kmeans = KMeans(n_clusters=cluster_count, random_state=0).fit(data)
+    # c = computeX(kmeans.cluster_centers_, table.get("mean"), table.get("std"))
+    # print("-----Labels------")
+    # print(kmeans.labels_)
+
+    kmeans = KMeans(n_clusters=cluster_count, random_state=0).fit(r)
+    cluster_details = getTableValues(r, kmeans.cluster_centers_, kmeans.labels_)
+    cluster_attributes = request.form.get("checked")
+    # points = {}
+    # if(len(clusters) == 2):
+    #     points = {
+    #         "labels": kmeans.labels_.tolist()
+    #     }
+
+
+
+    return render_template("index.html", clusters={
+        "cluster_details": cluster_details,
+        "cluster_attributes": cluster_attributes,
+        "attr_count": len(clusters),
+        "cluster_centroids": kmeans.cluster_centers_.tolist()
+    })
 
 # add a rule for the index page.
 application.add_url_rule('/', 'index', view_index)
--- Assignment 5/eb-flask/try.py
+++ Assignment 5/eb-flask/try.py
@@ -2,7 +2,21 @@ from sklearn.cluster import KMeans
 import numpy as np
 X = np.array([[1, 2], [1, 4], [1, 0],
                [10, 2], [10, 4], [10, 0]])
-kmeans = KMeans(n_clusters=2, random_state=0).fit(X)
+
+# print(X)
+# mean_X = np.mean(X, axis=0)
+# print("Mean: ")
+# print(mean_X)
+
+# std_X = np.std(X, axis=0)
+# print("Std: ")
+# print(std_X)
+
+# new_X = (X-mean_X)/std_X
+# print("New X: ")
+# print(new_X)
+
+kmeans = KMeans(n_clusters=3, random_state=0).fit(X)
 print(kmeans.labels_)
 # array([1, 1, 1, 0, 0, 0], dtype=int32)
 print(kmeans.predict([[0, 0], [12, 3]]))
@@ -10,3 +24,21 @@ print(kmeans.predict([[0, 0], [12, 3]]))
 print(kmeans.cluster_centers_)
 # array([[10.,  2.],
     #    [ 1.,  2.]])
+
+
+def getGraphValues(data, clusters, labels):
+    cluster_details = []
+    cluster_count = np.shape(clusters)[0]
+    for i in range(cluster_count):
+        this_cluster_indices = np.where(labels == i)[0]
+        print(this_cluster_indices)
+        this_cluster_points = data[this_cluster_indices,:]
+        this_distance = np.sqrt(np.sum((this_cluster_points - clusters[i,:])**2))
+        cluster_details.append({"Centroid": str(clusters[i,:]), "Closely Packed": str(this_distance), "Cluster Size": np.shape(this_cluster_points)[1]})
+        # print("Centroid: " + str(clusters[i,:]) + "..... Closely Packed: "+ str(this_distance) + "..... Cluster Size: " + str(np.shape(this_cluster_points)[1]))
+        # print(np.shape(this_cluster_points))
+
+    return cluster_details
+
+
+getGraphValues(X, kmeans.cluster_centers_, kmeans.labels_)
