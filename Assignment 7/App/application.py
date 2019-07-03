3f7d10cd88040cf07823f4de222cb648383004ae
Naman Jain <namanjain.vimalkumar@gmail.com>
2019-07-03 15:42:19 -0500

    starting quiz 7

--- Assignment 7/App/application.py
+++ Assignment 7/App/application.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import os
 import json
 import mysql.connector
+import redis
 
 db = None
 
@@ -9,6 +10,14 @@ if os.path.isfile('credentials.json'):
     with open('credentials.json') as f:
         cred = json.load(f)
         db = cred['database'][0]
+
+        # redis_cred = cred['redis'][0]
+        # r = redis.StrictRedis(host=redis_cred['endpoint'], port=redis_cred['port'], db=0)
+
+        # print(r)
+        # r.set("S", "Shruthi")
+        # response = r.get("S")
+        # print(response)
 
 else:
     print("credentials JSON not initialized")
@@ -24,12 +33,34 @@ def get_DbInstance():
     )
     return mydb
 
-application = Flask(__name__)
+def add_enrollment(student_id, course_num, section_num):
+
+    # Find max enrollments for that class
+    q1 = "SELECT max FROM classes where course = " + str(course_num) + " and section = " + str(section_num)
+    max_count = search_query(q1)[0]['max']
+
+    # Find current number of students in that class 
+    q2 = "SELECT COUNT(*) AS count FROM enrollment WHERE course_num = " + str(course_num) + " and section_num = " + str(course_num)
+    cur_count = search_query(q2)[0]['count']
+
+    # Add to enrollments
+    if(max_count > cur_count):
+        q3 = "INSERT INTO enrollment (course_num, section_num, student_id) VALUES (%s, %s, %s)"
+        val = (course_num, section_num, student_id)
+        mydb = get_DbInstance()
+        mycursor = mydb.cursor()
+        mycursor.execute(q3, val)
+        mydb.commit()
+        mydb.close()
+        return True
+    else:
+        return False
 
 def execute_query(query):
     mydb = get_DbInstance()
     mycursor = mydb.cursor()
     mycursor.execute(query)
+    mydb.commit()
     mydb.close()
     return mycursor.fetchall()
 
@@ -44,41 +75,38 @@ def search_query(query):
         json_data.append(dict(zip(row_headers,result)))
     return json_data
 
+application = Flask(__name__)
 
 def view_index():
-    table = {
-        "records": search_query("SELECT * FROM titanic3 LIMIT 10"),
-        "title": "titanic3 Table"
-    }
-    table1 = {
-        "records" : [{"name": "nike", "age": 10},{"name": "naman", "age": 20}],
-        "title": "Dict"
-    }
-    table2 = {
-        "records" : [{"class": "fucked up"}, {"class" : "hello world"}],
-        "title": "Dict2"
-    }
-    tables = [table1, table2, table]
-    return render_template("index.html", tables=tables)
-
-def post_view_handler():
-    cluster_count = int(request.form.get("cluster_count"))
-    checked = request.form.get("checked")
-    return render_template("index.html")
-
-def get_view_handler():
-    cluster_count = int(request.args.get("cluster_count"))
-    checked = request.args.get("checked")
     return render_template("index.html")
 
 # add a rule for the index page.
 application.add_url_rule('/', 'index', view_index)
 
-# application.add_url_rule('/api/clustering', 'postcluster', post_view_handler, methods=["POST"])
-
-# application.add_url_rule('/api/clustering', 'getcluster', get_view_handler, methods=["GET"])
 
 # run the app.
 if __name__ == "__main__":
     application.debug = True
     application.run()
+
+# Syntax
+
+    # table2 = {
+    #     "records" : [{"class": "fucked up"}, {"class" : "hello world"}],
+    #     "title": "Dict2"
+    # }
+    # return render_template("index.html", tables=[table2], msg = "Hello world")
+
+    # application.add_url_rule('/api/clustering', 'postcluster', post_view_handler, methods=["POST"])
+
+    # application.add_url_rule('/api/clustering', 'getcluster', get_view_handler, methods=["GET"])
+
+    # def post_view_handler():
+    #     cluster_count = int(request.form.get("cluster_count"))
+    #     checked = request.form.get("checked")
+    #     return render_template("index.html")
+
+    # def get_view_handler():
+    #     cluster_count = int(request.args.get("cluster_count"))
+    #     checked = request.args.get("checked")
+    #     return render_template("index.html")
--- Assignment 7/App/try.py
+++ Assignment 7/App/try.py
@@ -1,71 +1,14 @@
-from numpy import array
-from numpy import argmax
-from sklearn.preprocessing import LabelEncoder
-# define example
-data = ['cold', 'cold', 'warm', 'cold', 'hot', 'hot', 'warm', 'cold', 'warm', 'hot']
-values = array(data)
-print(values)
-# integer encode
-label_encoder = LabelEncoder()
-integer_encoded = label_encoder.fit_transform(values)
-print(integer_encoded)
-# binary encode
-# onehot_encoder = OneHotEncoder(sparse=False)
-# integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
-# onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
-# print(onehot_encoded)
-# # invert first example
-# inverted = label_encoder.inverse_transform([argmax(onehot_encoded[0, :])])
-# print(inverted)
+CREATE TABLE enrollment (
+    id int(10) PRIMARY KEY AUTO_INCREMENT,
+    course_num int(10),
+    student_id int(10),
+    section_num int(10)
+)
 
+# My courses
+SELECT * FROM classes as c
+LEFT OUTER JOIN enrollment as e ON c.course = e.course_num AND c.section = e.section_num
+WHERE e.student_id = 10012
 
-# from sklearn.cluster import KMeans
-# import numpy as np
-# X = np.array([[1, 2], [1, 4], [1, 0],
-#                [10, 2], [10, 4], [10, 0]])
-
-# # print(X)
-# # mean_X = np.mean(X, axis=0)
-# # print("Mean: ")
-# # print(mean_X)
-
-# # std_X = np.std(X, axis=0)
-# # print("Std: ")
-# # print(std_X)
-
-# # new_X = (X-mean_X)/std_X
-# # print("New X: ")
-# # print(new_X)
-
-# kmeans = KMeans(n_clusters=3, random_state=0).fit(X)
-# print(kmeans.labels_)
-# # array([1, 1, 1, 0, 0, 0], dtype=int32)
-# print(kmeans.predict([[0, 0], [12, 3]]))
-# # array([1, 0], dtype=int32)
-# print(kmeans.cluster_centers_)
-# # array([[10.,  2.],
-#     #    [ 1.,  2.]])
-
-
-# def getGraphValues(data, clusters, labels):
-#     cluster_details = []
-#     cluster_count = np.shape(clusters)[0]
-#     for i in range(cluster_count):
-#         this_cluster_indices = np.where(labels == i)[0]
-#         this_cluster_points = data[this_cluster_indices,:]
-#         this_distance = np.sqrt(np.sum((this_cluster_points - clusters[i,:])**2))
-#         print("--------- "+str(i))
-#         print(this_cluster_indices)
-#         print(clusters[i,:])
-#         print((((this_cluster_points - clusters[i,:])**2)))
-#         print("=====")
-#         this_count = np.shape(this_cluster_points)[1]
-#         print((np.sum(np.sqrt(np.sum((this_cluster_points - clusters[i,:])**2, axis=1))))/this_count)
-#         cluster_details.append({"Centroid": str(clusters[i,:]), "Closely Packed": str(this_distance), "Cluster Size": np.shape(this_cluster_points)[1]})
-#         # print("Centroid: " + str(clusters[i,:]) + "..... Closely Packed: "+ str(this_distance) + "..... Cluster Size: " + str(np.shape(this_cluster_points)[1]))
-#         # print(np.shape(this_cluster_points))
-
-#     return cluster_details
-
-
-# getGraphValues(X, kmeans.cluster_centers_, kmeans.labels_)
+# My details
+SELECT * FROM students WHERE IdNum = 10012
